fb5a414cac647b3f3d3529d4e500f962f66f04ec
sylhare <sylhare@outlook.com>
2025-04-15 09:27:21 -0400

    Update plugin documentation

--- docs/_plugins/simple_search_filter.rb
+++ docs/_plugins/simple_search_filter.rb
@@ -1,3 +1,12 @@
+# Example usage in a Jekyll template:
+#   {{ some_variable | remove_chars }}
+#
+# This will:
+#   - Replace backslashes (\) with `&#92;`
+#   - Replace tabs (\t) with four spaces
+#   - Remove control characters and extended ASCII characters
+#
+# Not compatible with GitHub Pages unless pre-built
 module Jekyll
   module CharFilter
     def remove_chars(input)
@@ -10,6 +19,7 @@ end
 
 Liquid::Template.register_filter(Jekyll::CharFilter)
 
+# This ensures the resulting string contains only printable ASCII characters.
 class String
   def strip_control_and_extended_characters()
     chars.each_with_object("") do |char, str|
--- docs/_plugins/simple_search_filter_cn.rb
+++ docs/_plugins/simple_search_filter_cn.rb
@@ -1,18 +1,29 @@
+# Same as `simple_search_filter.rb`, but with additional adapted for Chinese characters
+# Example usage in a Jekyll template:
+#   {{ some_variable | remove_chars_cn }}
+#
+# This will remove the following characters from `some_variable`:
+#   - Backslashes (\) are replaced with `&#92;`
+#   - Tabs (\t) are replaced with four spaces
+#   - The following characters are removed: @, $, %, &, ", {, }
+#
+# Not compatible with GitHub page unless pre-built
 module Jekyll
   module CharFilter
     def remove_chars_cn(input)
-      input.gsub! '\\','&#92;'
+      input.gsub! '\\', '&#92;'
       input.gsub! /\t/, '    '
-	  input.gsub! '@',''
-	  input.gsub! '$',''
-	  input.gsub! '%',''
-	  input.gsub! '&',''
-	  input.gsub! '"',''
-	  input.gsub! '{',''
-	  input.gsub! '}',''
-	  input
+      input.gsub! '@', ''
+      input.gsub! '$', ''
+      input.gsub! '%', ''
+      input.gsub! '&', ''
+      input.gsub! '"', ''
+      input.gsub! '{', ''
+      input.gsub! '}', ''
+      input
     end
   end
 end
 
+# Registers the `remove_chars_cn` filter for use in Jekyll templates.
 Liquid::Template.register_filter(Jekyll::CharFilter)
