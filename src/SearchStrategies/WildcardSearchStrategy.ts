e7dbe621c5225f6f86f6be263d827023b9f2a517
sylhare <sylhare@outlook.com>
2025-04-18 07:02:17 -0400

    Add WildcardSearchStrategy

---
+++ src/SearchStrategies/WildcardSearchStrategy.ts
@@ -0,0 +1,6 @@
+import { SearchStrategy } from './types';
+import { wildcardFuzzySearch } from '../utils/wildcardFuzzySearch';
+
+export class WildcardSearchStrategy implements SearchStrategy {
+}
+
