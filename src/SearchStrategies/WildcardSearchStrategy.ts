b128aff377a9f8bc1074736d6b783207c90e4137
sylhare <sylhare@outlook.com>
2025-04-20 11:10:33 -0400

    Add wildcard search

--- src/SearchStrategies/WildcardSearchStrategy.ts
+++ src/SearchStrategies/WildcardSearchStrategy.ts
@@ -2,5 +2,13 @@ import { SearchStrategy } from './types';
 import { wildcardFuzzySearch } from '../utils/wildcardFuzzySearch';
 
 export class WildcardSearchStrategy implements SearchStrategy {
+  public matches(text: string | null, criteria: string): boolean {
+    if (text === null) {
+      return false;
+    }
+
+    return wildcardFuzzySearch(text, criteria);
+  }
 }
 
+export default new WildcardSearchStrategy(); 
