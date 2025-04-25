27dd68c4b0490906a957e9dcc6feab37575d7d9b
sylhare <sylhare@outlook.com>
2025-04-25 08:52:51 -0400

    Update version

--- dest/simple-jekyll-search.js
+++ dest/simple-jekyll-search.js
@@ -2,6 +2,211 @@
   typeof define === "function" && define.amd ? define(factory) : factory();
 })(function() {
   "use strict";
+  function merge(target, source) {
+    return { ...target, ...source };
+  }
+  function isJSON(json) {
+    try {
+      return !!(json instanceof Object && JSON.parse(JSON.stringify(json)));
+    } catch (_err) {
+      return false;
+    }
+  }
+  function NoSort() {
+    return 0;
+  }
+  function isObject(obj) {
+    return Boolean(obj) && Object.prototype.toString.call(obj) === "[object Object]";
+  }
+  function clone(input) {
+    if (input === null || typeof input !== "object") {
+      return input;
+    }
+    if (Array.isArray(input)) {
+      return input.map((item) => clone(item));
+    }
+    const output = {};
+    for (const key in input) {
+      if (Object.prototype.hasOwnProperty.call(input, key)) {
+        output[key] = clone(input[key]);
+      }
+    }
+    return output;
+  }
+  const DEFAULT_OPTIONS = {
+    searchInput: null,
+    resultsContainer: null,
+    json: [],
+    success: function() {
+    },
+    searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
+    templateMiddleware: (_prop, _value, _template) => void 0,
+    sortMiddleware: NoSort,
+    noResultsText: "No results found",
+    limit: 10,
+    fuzzy: false,
+    debounceTime: null,
+    exclude: [],
+    onSearch: () => {
+    }
+  };
+  const REQUIRED_OPTIONS = ["searchInput", "resultsContainer", "json"];
+  const WHITELISTED_KEYS = /* @__PURE__ */ new Set([13, 16, 20, 37, 38, 39, 40, 91]);
+  class SearchStrategy {
+    constructor(matchFunction) {
+      this.matchFunction = matchFunction;
+    }
+    matches(text, criteria) {
+      if (text === null || text.trim() === "" || !criteria) {
+        return false;
+      }
+      return this.matchFunction(text, criteria);
+    }
+  }
+  function literalSearch(text, criteria) {
+    text = text.trim().toLowerCase();
+    const pattern = criteria.endsWith(" ") ? [criteria.toLowerCase()] : criteria.trim().toLowerCase().split(" ");
+    return pattern.filter((word) => text.indexOf(word) >= 0).length === pattern.length;
+  }
+  function levenshtein(a, b) {
+    const lenA = a.length;
+    const lenB = b.length;
+    const distanceMatrix = Array.from({ length: lenA + 1 }, () => Array(lenB + 1).fill(0));
+    for (let i = 0; i <= lenA; i++) distanceMatrix[i][0] = i;
+    for (let j = 0; j <= lenB; j++) distanceMatrix[0][j] = j;
+    for (let i = 1; i <= lenA; i++) {
+      for (let j = 1; j <= lenB; j++) {
+        const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+        distanceMatrix[i][j] = Math.min(
+          distanceMatrix[i - 1][j] + 1,
+          // Removing a character from one string
+          distanceMatrix[i][j - 1] + 1,
+          // Adding a character to one string to make it closer to the other string.
+          distanceMatrix[i - 1][j - 1] + cost
+          // Replacing one character in a string with another
+        );
+      }
+    }
+    return distanceMatrix[lenA][lenB];
+  }
+  function levenshteinSearch(text, pattern) {
+    const distance = levenshtein(pattern, text);
+    const similarity = 1 - distance / Math.max(pattern.length, text.length);
+    return similarity >= 0.3;
+  }
+  function wildcardSearch(text, pattern) {
+    const regexPattern = pattern.replace(/\*/g, ".*");
+    const regex = new RegExp(`^${regexPattern}$`, "i");
+    if (regex.test(text)) return true;
+    return levenshteinSearch(text, pattern);
+  }
+  function fuzzySearch(text, pattern) {
+    pattern = pattern.trimEnd();
+    if (pattern.length === 0) return true;
+    pattern = pattern.toLowerCase();
+    text = text.toLowerCase();
+    let remainingText = text, currentIndex = -1;
+    for (const char of pattern) {
+      const nextIndex = remainingText.indexOf(char);
+      if (nextIndex === -1 || currentIndex !== -1 && remainingText.slice(0, nextIndex).split(" ").length - 1 > 2) {
+        return false;
+      }
+      currentIndex = nextIndex;
+      remainingText = remainingText.slice(nextIndex + 1);
+    }
+    return true;
+  }
+  const LiteralSearchStrategy = new SearchStrategy(literalSearch);
+  const FuzzySearchStrategy = new SearchStrategy((text, criteria) => {
+    return fuzzySearch(text, criteria) || literalSearch(text, criteria);
+  });
+  const WildcardSearchStrategy = new SearchStrategy((text, criteria) => {
+    return wildcardSearch(text, criteria) || literalSearch(text, criteria);
+  });
+  class Repository {
+    constructor(initialOptions = {}) {
+      this.data = [];
+      this.setOptions(initialOptions);
+    }
+    put(input) {
+      if (isObject(input)) {
+        return this.addObject(input);
+      }
+      if (Array.isArray(input)) {
+        return this.addArray(input);
+      }
+      return void 0;
+    }
+    clear() {
+      this.data.length = 0;
+      return this.data;
+    }
+    search(criteria) {
+      if (!criteria) {
+        return [];
+      }
+      return clone(this.findMatches(this.data, criteria).sort(this.options.sortMiddleware));
+    }
+    setOptions(newOptions) {
+      this.options = {
+        fuzzy: (newOptions == null ? void 0 : newOptions.fuzzy) || false,
+        limit: (newOptions == null ? void 0 : newOptions.limit) || DEFAULT_OPTIONS.limit,
+        searchStrategy: this.searchStrategy((newOptions == null ? void 0 : newOptions.strategy) || newOptions.fuzzy && "fuzzy"),
+        sortMiddleware: (newOptions == null ? void 0 : newOptions.sortMiddleware) || DEFAULT_OPTIONS.sortMiddleware,
+        exclude: (newOptions == null ? void 0 : newOptions.exclude) || DEFAULT_OPTIONS.exclude
+      };
+    }
+    addObject(obj) {
+      this.data.push(obj);
+      return this.data;
+    }
+    addArray(arr) {
+      const added = [];
+      this.clear();
+      for (const item of arr) {
+        if (isObject(item)) {
+          added.push(this.addObject(item)[0]);
+        }
+      }
+      return added;
+    }
+    findMatches(data, criteria) {
+      const matches = [];
+      for (let i = 0; i < data.length && matches.length < this.options.limit; i++) {
+        const match = this.findMatchesInObject(data[i], criteria);
+        if (match) {
+          matches.push(match);
+        }
+      }
+      return matches;
+    }
+    findMatchesInObject(obj, criteria) {
+      for (const key in obj) {
+        if (!this.isExcluded(obj[key]) && this.options.searchStrategy.matches(obj[key], criteria)) {
+          return obj;
+        }
+      }
+      return void 0;
+    }
+    isExcluded(term) {
+      for (const excludedTerm of this.options.exclude) {
+        if (new RegExp(excludedTerm).test(String(term))) {
+          return true;
+        }
+      }
+      return false;
+    }
+    searchStrategy(strategy) {
+      switch (strategy) {
+        case "fuzzy":
+          return FuzzySearchStrategy;
+        case "wildcard":
+          return WildcardSearchStrategy;
+        default:
+          return LiteralSearchStrategy;
+      }
+    }
+  }
   function load(location, callback) {
     const xhr = getXHR();
     xhr.open("GET", location, true);
@@ -48,124 +253,6 @@
       return typeof params.required !== "undefined" && Array.isArray(params.required);
     }
   }
-  function fuzzySearch(pattern, text) {
-    pattern = pattern.trimEnd();
-    if (pattern.length === 0) return true;
-    if (text.length === 0) return false;
-    pattern = pattern.toLowerCase();
-    text = text.toLowerCase();
-    let remainingText = text, currentIndex = -1;
-    for (const char of pattern) {
-      const nextIndex = remainingText.indexOf(char);
-      if (nextIndex === -1 || currentIndex !== -1 && remainingText.slice(0, nextIndex).split(" ").length - 1 > 2) {
-        return false;
-      }
-      currentIndex = nextIndex;
-      remainingText = remainingText.slice(nextIndex + 1);
-    }
-    return true;
-  }
-  class FuzzySearchStrategy {
-    matches(text, criteria) {
-      if (text === null) {
-        return false;
-      }
-      return fuzzySearch(criteria, text);
-    }
-  }
-  class LiteralSearchStrategy {
-    matches(text, criteria) {
-      if (!text) return false;
-      text = text.trim().toLowerCase();
-      const pattern = criteria.endsWith(" ") ? [criteria.toLowerCase()] : criteria.trim().toLowerCase().split(" ");
-      return pattern.filter((word) => text.indexOf(word) >= 0).length === pattern.length;
-    }
-  }
-  function NoSort() {
-    return 0;
-  }
-  const data = [];
-  let opt = {
-    limit: 10,
-    searchStrategy: new FuzzySearchStrategy(),
-    sort: NoSort,
-    exclude: []
-  };
-  function put(input) {
-    if (isObject(input)) {
-      return addObject(input);
-    }
-    if (isArray(input)) {
-      return addArray(input);
-    }
-    return void 0;
-  }
-  function clear() {
-    data.length = 0;
-    return data;
-  }
-  function isObject(obj) {
-    return Boolean(obj) && Object.prototype.toString.call(obj) === "[object Object]";
-  }
-  function isArray(obj) {
-    return Boolean(obj) && Object.prototype.toString.call(obj) === "[object Array]";
-  }
-  function addObject(_data) {
-    data.push(_data);
-    return data;
-  }
-  function addArray(_data) {
-    const added = [];
-    clear();
-    for (let i = 0, len = _data.length; i < len; i++) {
-      if (isObject(_data[i])) {
-        added.push(addObject(_data[i])[0]);
-      }
-    }
-    return added;
-  }
-  function search$1(criteria) {
-    if (!criteria) {
-      return [];
-    }
-    return findMatches(data, criteria, opt.searchStrategy, opt).sort(opt.sort);
-  }
-  function setOptions$1(_opt) {
-    opt = {
-      fuzzy: _opt.fuzzy || false,
-      limit: _opt.limit || 10,
-      searchStrategy: _opt.fuzzy ? new FuzzySearchStrategy() : new LiteralSearchStrategy(),
-      sort: _opt.sort || NoSort,
-      exclude: _opt.exclude || []
-    };
-  }
-  function findMatches(data2, criteria, strategy, opt2) {
-    const matches = [];
-    for (let i = 0; i < data2.length && matches.length < opt2.limit; i++) {
-      const match = findMatchesInObject(data2[i], criteria, strategy, opt2);
-      if (match) {
-        matches.push(match);
-      }
-    }
-    return matches;
-  }
-  function findMatchesInObject(obj, criteria, strategy, opt2) {
-    for (const key in obj) {
-      if (!isExcluded(obj[key], opt2.exclude) && strategy.matches(obj[key], criteria)) {
-        return obj;
-      }
-    }
-    return void 0;
-  }
-  function isExcluded(term, excludedTerms) {
-    for (let i = 0, len = excludedTerms.length; i < len; i++) {
-      const excludedTerm = excludedTerms[i];
-      if (new RegExp(excludedTerm).test(String(term))) {
-        return true;
-      }
-    }
-    return false;
-  }
   const options$1 = {
     pattern: /\{(.*?)\}/g,
     template: "",
@@ -184,53 +271,18 @@
       options$1.middleware = _options.middleware;
     }
   }
-  function compile(data2) {
+  function compile(data) {
     return options$1.template.replace(options$1.pattern, function(match, prop) {
-      const value = options$1.middleware(prop, data2[prop], options$1.template);
+      const value = options$1.middleware(prop, data[prop], options$1.template);
       if (typeof value !== "undefined") {
         return value;
       }
-      return data2[prop] || match;
+      return data[prop] || match;
     });
   }
-  function merge(defaultParams, mergeParams) {
-    const mergedOptions = {};
-    for (const option in defaultParams) {
-      mergedOptions[option] = defaultParams[option];
-      if (typeof mergeParams[option] !== "undefined") {
-        mergedOptions[option] = mergeParams[option];
-      }
-    }
-    return mergedOptions;
-  }
-  function isJSON(json) {
-    try {
-      return !!(json instanceof Object && JSON.parse(JSON.stringify(json)));
-    } catch (_err) {
-      return false;
-    }
-  }
-  const DEFAULT_OPTIONS = {
-    searchInput: null,
-    resultsContainer: null,
-    json: [],
-    success: function() {
-    },
-    searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
-    templateMiddleware: (_prop, _value, _template) => void 0,
-    sortMiddleware: () => 0,
-    noResultsText: "No results found",
-    limit: 10,
-    fuzzy: false,
-    debounceTime: null,
-    exclude: [],
-    onSearch: () => {
-    }
-  };
-  const REQUIRED_OPTIONS = ["searchInput", "resultsContainer", "json"];
-  const WHITELISTED_KEYS = /* @__PURE__ */ new Set([13, 16, 20, 37, 38, 39, 40, 91]);
   let options = { ...DEFAULT_OPTIONS };
   let debounceTimerHandle;
+  const repository = new Repository();
   const optionsValidator = new OptionsValidator({
     required: REQUIRED_OPTIONS
   });
@@ -258,7 +310,7 @@
     return !WHITELISTED_KEYS.has(key);
   };
   const initWithJSON = (json) => {
-    put(json);
+    repository.put(json);
     registerInput();
   };
   const initWithURL = (url) => {
@@ -284,8 +336,7 @@
     var _a;
     if (isValidQuery(query)) {
       emptyResultsContainer();
-      const results = search$1(query);
-      render(results, query);
+      render(repository.search(query), query);
       (_a = options.onSearch) == null ? void 0 : _a.call(options);
     }
   };
@@ -314,10 +365,11 @@
       template: options.searchResultTemplate,
       middleware: options.templateMiddleware
     });
-    setOptions$1({
+    repository.setOptions({
       fuzzy: options.fuzzy,
       limit: options.limit,
-      sort: options.sortMiddleware,
+      sortMiddleware: options.sortMiddleware,
+      strategy: options.strategy,
       exclude: options.exclude
     });
     if (isJSON(options.json)) {
