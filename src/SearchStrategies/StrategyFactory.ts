502441bef2aa6ae196aaf975321dab39c57de667
sylhare <sylhare@outlook.com>
2026-08-14 13:16:57 -0400

    Harden wildcard priority and pin accepted literal/fuzzy divergences

--- src/SearchStrategies/StrategyFactory.ts
+++ src/SearchStrategies/StrategyFactory.ts
@@ -13,7 +13,7 @@ export class StrategyFactory {
         return new UnifiedSearchStrategy({ ...options, wildcardPriority: false, preferFuzzy: true, minFuzzyLength: 1, maxExtraFuzzyChars: Number.POSITIVE_INFINITY });
 
       case 'wildcard':
-        return new UnifiedSearchStrategy({ maxSpaces: 0, ...options, preferFuzzy: false, minFuzzyLength: Number.POSITIVE_INFINITY });
+        return new UnifiedSearchStrategy({ maxSpaces: 0, ...options, wildcardPriority: true, preferFuzzy: false, minFuzzyLength: Number.POSITIVE_INFINITY });
 
       case 'hybrid':
       case 'unified':
